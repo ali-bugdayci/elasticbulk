66fde75ec92d12bf9d70a6ec015a2acf97ed7e2d
Ali Buğdaycı <ali@iotiq.de>
2021-12-22 17:54:33 +0300

    Fix bug

--- src/helper.js
+++ src/helper.js
@@ -4,7 +4,7 @@ module.exports.log = options => {
 	return v => {
 		if (options.debug && v.errors) {
 			let erroredDocs = _.filter(v.items, i => { return i.create.status !== 201 });
-			console.log(JSON.stringify(v, null, 2));
+			console.log(JSON.stringify(erroredDocs, null, 2));
 		}
 	}
 }
