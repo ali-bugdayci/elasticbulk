130051c5ec37ea6de784bae48e95e0d414539af1
Ali Buğdaycı <ali@iotiq.de>
2021-12-27 18:07:35 +0300

    Make helper non ES version dependent

--- src/helper.js
+++ src/helper.js
@@ -3,8 +3,20 @@ let _ = require('lodash');
 module.exports.log = options => {
 	return v => {
 		if (options.debug && v.errors) {
-			let erroredDocs = _.filter(v.items, i => { return i.create.status !== 201 });
-			console.log(JSON.stringify(erroredDocs, null, 2));
+			let first = v.items[0];
+			let key = Object.keys(first)[0];
+
+			let erroredDocs = _.filter(v.items, i => { return i[key].status !== 201 });
+
+			console.log("Number of errored documents: " + erroredDocs.length);
+
+			var reasons = _.map(erroredDocs, e => {
+				let err = e[key].error;
+				return err.reason;
+			})
+			
+
+			console.log(JSON.stringify(_.uniq(reasons), null, 2));
 		}
 	}
 }
